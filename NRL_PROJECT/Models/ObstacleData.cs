2572ce941f6db25f90b431ce222cd557d898e5f6
haldvar <halli.jansen@gmail.com>
2025-09-12 13:14:19 +0200

    Added Overview, DataForm, ObstacleData, and made other changes.

---
+++ NRL_PROJECT/Models/ObstacleData.cs
@@ -0,0 +1,8 @@
+namespace NRL_PROJECT.Models;
+
+public class ObstacleData
+{
+    public string ObstacleName {get; set;}
+    public string ObstacleHeight {get; set;}
+    public string ObstacleDescription {get; set;}
+}
--- NRL_PROJECT/obj/Debug/net9.0/NRL_PROJECT.AssemblyInfo.cs
+++ NRL_PROJECT/obj/Debug/net9.0/NRL_PROJECT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NRL_PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5ac303d16759e0567674cbe920a5c878138e82")]
 [assembly: System.Reflection.AssemblyProductAttribute("NRL_PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NRL_PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
