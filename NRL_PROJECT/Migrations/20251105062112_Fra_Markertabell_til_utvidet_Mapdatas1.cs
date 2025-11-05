1421a4b83c16e5d77053eda173d9f058bc232be9
Karoline <kaas02@uia.no>
2025-11-05 07:24:02 +0100

    Migrert

---
+++ NRL_PROJECT/Migrations/20251105062112_Fra_Markertabell_til_utvidet_Mapdatas1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NRL_PROJECT.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fra_Markertabell_til_utvidet_Mapdatas1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude2",
+                table: "Obstacles",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude2",
+                table: "Obstacles",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude2",
+                table: "MapDatas",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude2",
+                table: "MapDatas",
+                type: "double",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude2",
+                table: "Obstacles",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude2",
+                table: "Obstacles",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude2",
+                table: "MapDatas",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude2",
+                table: "MapDatas",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double",
+                oldNullable: true);
+        }
+    }
+}
