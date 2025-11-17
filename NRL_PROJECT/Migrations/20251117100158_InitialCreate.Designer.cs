815a79d0ad517a9b7e6b9cf6332c9b4bcbbd2a47
Janke82 <jankemann@gmail.com>
2025-11-17 11:17:48 +0100

    Location, location
    Fikser for lokasjon, kart ui, autosave forbedra, egne ikoner, cancel knappen redirecter ikke, den tømmer skjema bare.

--- NRL_PROJECT/Migrations/20251117100158_InitialCreate.Designer.cs
+++ NRL_PROJECT/Migrations/20251117100158_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using NRL_PROJECT.Data;
 namespace NRL_PROJECT.Migrations
 {
     [DbContext(typeof(NRL_Db_Context))]
-    [Migration("20251115175805_InitialCreate")]
+    [Migration("20251117100158_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
